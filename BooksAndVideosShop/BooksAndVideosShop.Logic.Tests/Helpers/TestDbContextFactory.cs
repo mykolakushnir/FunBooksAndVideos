ba2716e43e33bb20ca97368cd21d775929afd167
mykolakushnir <44592773+mykolakushnir@users.noreply.github.com>
2025-04-22 06:29:53 +0300

    Added unit tests for business logic

---
+++ BooksAndVideosShop/BooksAndVideosShop.Logic.Tests/Helpers/TestDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using BooksAndVideosShop.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAndVideosShop.Logic.Tests.Helpers
+{
+    public static class TestDbContextFactory
+    {
+        public static ShopDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ShopDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()) 
+                .Options;
+
+            return new ShopDbContext(options);
+        }
+    }
+}
